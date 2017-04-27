0f3bdf250cbbfc0b1d16e9342a436990d1ffd282
fandul <rihards.pacevics@gmail.com>
2017-04-27 15:00:45 +0200

    added positive tweet router

--- server/routes/social.js
+++ server/routes/social.js
@@ -23,4 +23,15 @@ router.get('/numberOfUsers', function (req, res, next) {
 
 });
 
+router.get('/positiveTweets', function (req, res, next) {
+  tweet.aggregate([
+      { $match: { polarity: 4 } },
+      { $group: { _id: '$user', count: { $sum: 1 } } },
+      { $sort: { count: -1 } },
+      { $limit: 5 }
+  ], function (err, data) {
+      res.json(data);
+    });
+});
+
 module.exports = router;
