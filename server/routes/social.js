b6b98c06211790601170468e064031938c4444fc
benthehorn <benhorn13@googlemail.com>
2017-04-29 11:46:39 +0200

    added route to get top ten mentioners

--- server/routes/social.js
+++ server/routes/social.js
@@ -9,7 +9,7 @@ var sanitizeHtml = require('sanitize-html');
 var db = require('../config/db');
 var tweet = require('../models/social');
 
-router.get('/one/', function (req, res, next) {
+router.get('/one', function (req, res, next) {
   tweet.find({ user: '_TheSpecialOne_' }, function (err, data) {
     res.send(data);
   });
@@ -23,15 +23,32 @@ router.get('/numberOfUsers', function (req, res, next) {
 
 });
 
+
 router.get('/positiveTweets', function (req, res, next) {
   tweet.aggregate([
-      { $match: { polarity: 4 } },
-      { $group: { _id: '$user', count: { $sum: 1 } } },
-      { $sort: { count: -1 } },
-      { $limit: 5 }
+    { $match: { polarity: 4 } },
+    { $group: { _id: '$user', count: { $sum: 1 } } },
+    { $sort: { count: -1 } },
+    { $limit: 5 }
   ], function (err, data) {
-      res.json(data);
-    });
+    res.json(data);
+  });
+});
+
+router.get('/userNames', function (req, res, next) {
+  var mentions = [];
+  //add mentions: {$mentions} to get the manes of the mentioned people
+  tweet.aggregate([
+      { $unwind: '$mentions' },
+      { $group: { _id: '$user',  size: { $sum: 1 } } },
+      { $sort: { size: -1 } },
+      { $limit: 10 }]).allowDiskUse(true).exec(function (err, data) {
+    if (err) {
+      return console.log('error', err);
+    }
+
+    res.json(data);
+  });
 });
 
 module.exports = router;
